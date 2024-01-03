3b8a1727809e0dbb9a7a624d5e305cebbb9c5032
asepsobarudin <asepsobarudin066@gmail.com>
2024-01-03 11:42:57 +0700

    fix(dashboard):fix bug date error

--- app/Views/pages/dashboard/dashboard_page.php
+++ app/Views/pages/dashboard/dashboard_page.php
@@ -11,18 +11,18 @@ $day =  date('d');
 $dayName =  date('l');
 
 $bulan = array(
-  1 =>   'Januari',
-  2 => 'Februari',
-  3 => 'Maret',
-  4 => 'April',
-  5 => 'Mei',
-  6 => 'Juni',
-  7 => 'Juli',
-  8 => 'Agustus',
-  9 => 'September',
-  10 => 'Oktober',
-  11 => 'November',
-  12 => 'Desember'
+  '01' =>  'Januari',
+  '02' => 'Februari',
+  '03' => 'Maret',
+  '04' => 'April',
+  '05' => 'Mei',
+  '06' => 'Juni',
+  '07' => 'Juli',
+  '08' => 'Agustus',
+  '09' => 'September',
+  '10' => 'Oktober',
+  '11' => 'November',
+  '12' => 'Desember'
 );
 
 $hari = [
